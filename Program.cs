b1e565f530beb90d0ef23ce30d1de4ddc4cac31d
Luana Duarte <75185024+luana-duarte@users.noreply.github.com>
2021-07-03 18:05:17 -0300

    atualizado

--- Program.cs
+++ Program.cs
@@ -7,17 +7,20 @@ namespace Menu
     {
         static void Main(string[] args)
         {
-            List<string> nomes = new();
-            Console.WriteLine("1 - Digite um nome");
-            Console.WriteLine("2 - Ler nome");
-            Console.WriteLine("3 - Insira um nome e altere a lista");
-            Console.WriteLine("4 - Excluir nome");
-            Console.WriteLine("5 - Lista atualizada");
-            Console.WriteLine("6 - Sair");
-            Console.WriteLine("Digite qualquer número para continuar");
-            int opc = int.Parse(Console.ReadLine()); 
+            List<string> nomes = new() { "Luana", "Leticia" };
+
+            Console.WriteLine("Digite qualquer número de para continuar");
+            int opc = int.Parse(Console.ReadLine());
             while (opc != 6)
             {
+                Console.WriteLine();
+                Console.WriteLine("1 - Digite um nome");
+                Console.WriteLine("2 - Ler nome");
+                Console.WriteLine("3 - Insira um nome e altere a lista");
+                Console.WriteLine("4 - Excluir nome");
+                Console.WriteLine("5 - Lista atualizada");
+                Console.WriteLine("6 - Sair");
+                Console.WriteLine();
                 Console.Write("Digite a opção desejada:     ");
                 opc = int.Parse(Console.ReadLine());
                 switch (opc)
@@ -35,10 +38,37 @@ namespace Menu
                         break;
 
                     case 3:
-                        Console.Write("Insira um nome e altere a lista: ");
-                        nomes.Insert(0, Console.ReadLine());
-                        break;
+                        Console.Write("Qual nome deseja alterar: ");
+                        string nome = Console.ReadLine();
+                        int x = new int();
 
+                        for (int i = 0; i < nomes.Count; i++)
+                        {
+                            if (nome == nomes[i])
+                            {
+                                x = i;
+                                Console.WriteLine("Esse nome já existe! ");
+
+                            }
+                            else if (nome != nomes[i])
+                            {
+                                Console.WriteLine("Digite um novo nome: ");
+                                string n = Console.ReadLine();
+
+                                if (i == nomes.Count - 1 && !nomes.Contains(" ") && n != null && n.Length > 0)
+                                {
+                                    nomes[x] = n;
+                                    Console.WriteLine("Nome alterado!");
+
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Não foi possivel alterar!");
+                                }
+                            }
+                        }
+                        break;
                     case 4:
                         Console.Write("Exclua um nome: ");
                         nomes.Remove(Console.ReadLine());
@@ -61,3 +91,4 @@ namespace Menu
 
     }
 }
+
